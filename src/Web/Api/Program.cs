33965a3ea1eb663d3d44f41b9875a22acb35eac4
Maxim Verreault <verreault.maxim@gmail.com>
2023-08-04 00:47:44 -0400

    setup health checks

--- src/Web/Api/Program.cs
+++ src/Web/Api/Program.cs
@@ -1,3 +1,5 @@
+using RiverRunner.WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Setup health checks
+builder.Services.AddCustomHealthChecks();
 
 var app = builder.Build();
 
@@ -21,5 +25,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCustomHealthChecks();
 
 app.Run();
